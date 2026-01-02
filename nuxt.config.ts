89f6f4e9304308cd1d5c889d3b0ea829425f3910
botent <anilkumar.edc@gmail.com>
2026-01-02 15:34:30 +0000

    Add app head configuration with Umami script for analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,15 @@ export default defineNuxtConfig({
   llms: {
     domain: 'docs.tryrice.com',
   },
+  app: {
+    head: {
+      script: [
+        {
+          defer: true,
+          src: 'https://cloud.umami.is/script.js',
+          'data-website-id': '617904df-11ac-44ca-9878-79b6fcb7c1cc',
+        },
+      ],
+    },
+  },
 })
